d114304eec4cc54ad5f76d755bb01b12fab71536
Wataru TOrii <eagle.hokkaido2317@gmail.com>
2011-12-08 10:12:09 +0900

    2011/12/8

---
+++ Gragh.hs
@@ -0,0 +1,40 @@
+module Gragh where
+
+import Graphics.UI.GLUT
+import Data.IORef
+
+data GraghInfo
+  = GraghInfo {
+        width :: Float,
+        height:: Float,
+        func  :: Float -> Float,
+        fcolor:: Color3 GLint,
+        x_axis::Float,
+        y_axis::Float,
+        px    ::Float
+    }
+
+drawAxis :: Float -> Float -> Float -> Float -> IO()
+drawAxis w h x y
+ = do
+    preservingMatrix $ do	-- x軸描画
+      color (Color3 1.0 1.0 1.0 :: Color3 GLdouble)
+      renderPrimitive LineStrip $ mapM_ vertex
+       [ Vertex2 ((w-y)/w-0.5)  1.0,
+         Vertex2 ((w-y)/w-0.5) (-1.0) :: Vertex2 GLfloat]
+      renderPrimitive LineStrip $ mapM_ vertex
+       [ Vertex2  1.0   ((h-x)/h-0.5),
+         Vertex2 (-1.0) ((h-x)/h-0.5) :: Vertex2 GLfloat]
+
+
+drawGragh :: GraghInfo -> IO()
+drawGragh info
+ = do
+      clear [ColorBuffer]
+
+      drawAxis (width info) (height info) (x_axis info) (y_axis info)
+     -- preservingMatrix $ do
+     --   color (fcolor info)
+     --   renderPrimitive Points $ mapM_ (vertex.uncurry)
+     --     [(0.5 0.5)]
+      swapBuffers
---
+++ main.hs
@@ -0,0 +1,19 @@
+import Gragh
+import Graphics.UI.GLUT
+
+test :: GraghInfo
+test = GraghInfo {width = 600,height = 400,
+                  func = sin ,fcolor = Color3 100 100 100,
+                  x_axis = 200,y_axis = 300 , px = 50}
+
+main :: IO()
+main
+ = do getArgsAndInitialize
+      initialWindowSize $= Size (floor $ width test) (floor $height test)
+      initialWindowPosition $= Position 100 100
+      initialDisplayMode $= [DoubleBuffered , RGBMode]
+
+      createWindow "gragh wigh Haskell"
+
+      displayCallback $= drawGragh test
+      mainLoop
